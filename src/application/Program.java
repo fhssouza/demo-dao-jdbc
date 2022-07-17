1b749e7658bb13039fec9c077872f7030c937f66
fhssouza <fhssouza@gmail.com>
2022-07-17 11:33:07 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,6 +23,12 @@ public class Program {
 		for(Seller obj : sellers) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 3: seller findByAll ===");
+		sellers = sellerDao.findAll();
+		for(Seller obj : sellers) {
+			System.out.println(obj);
+		}
 	}
 
 }
