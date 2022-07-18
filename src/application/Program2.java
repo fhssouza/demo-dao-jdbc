d00d0ddd06c5881164effdaa0c237d44171ea32c
fhssouza <fhssouza@gmail.com>
2022-07-18 15:17:50 -0300

    findAll, findById department

---
+++ src/application/Program2.java
@@ -0,0 +1,33 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		System.out.println("=== TEST 1: Department findById ===");
+		Department department = departmentDao.findById(1);
+		System.out.println(department);
+		
+		System.out.println("\n=== TEST 2: Department findByAll ===");
+		List<Department> departments = new ArrayList<>();
+		departments = departmentDao.findAll();
+		for(Department obj : departments) {
+			System.out.println(obj);
+		}
+		
+		sc.close();
+	}
+
+}
